25934534c40807bcd621469628ba85367c5c0ea1
vanshrajput099 <vanshrajput099@gmail.com>
2023-06-13 13:58:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,499 @@
+const quiz = [
+    {
+    "question" : "They fly at night; you'd better run. These winged things are not much fun.",
+    "option1" : "Birds",
+    "option2" : "Bats",
+    "option3" : "Eagles",
+    "option4" : "Witches",
+    "correct" : "Bats",
+    "voice": "./voices/ques-1.mp3"
+    },
+
+    {
+    "question" : "A little bite will make you itch, make you sneeze, make you twitch.",
+    "option1" : "Mosquito",
+    "option2" : "Hamburgers",
+    "option3" : "Ant",
+    "option4" : "Nick",
+    "correct" : "Mosquito",
+    "voice": "./voices/ques-2.mp3"
+    },
+
+    {
+    "question" : "​His fangs are sharp; he likes your taste.",
+    "option1" : "Bat",
+    "option2" : "Vampire",
+    "option3" : "Mosquito",
+    "option4" : "Lion",
+    "correct" : "Lion",
+    "voice": "./voices/ques-3.mp3"
+    },
+
+    {
+    "question" : "They grow much faster than bamboo. Take care, or they'll come after you.",
+    "option1" : "Palm",
+    "option2" : "Plants",
+    "option3" : "Tree",
+    "option4" : "Ants",
+    "correct" : "Plants",
+    "voice": "./voices/ques-4.mp3"
+    },
+
+    {
+    "question" : "Don't be fooled; it isn't thunder. Staying put would be a blunder.",
+    "option1" : "Stampede",
+    "option2" : "Horses",
+    "option3" : "Earthquake",
+    "option4" : "Arrows",
+    "correct" : "Stampede",
+    "voice": "./voices/ques-5.mp3"
+    },
+
+    {
+    "question" : "Need a hand? Well, you just wait. We'll lend a hand; we each have eight.",
+    "option1" : "Octopuses",
+    "option2" : "Spiders",
+    "option3" : "Ants",
+    "option4" : "Roaches",
+    "correct" : "Spiders",
+    "voice": "./voices/ques-6.mp3"
+    },
+
+    {
+    "question" : "You're almost there with much at stake. But now, the ground begins to quake.",
+    "option1" : "Stampede",
+    "option2" : "Earthquake",
+    "option3" : "War",
+    "option4" : "Fire",
+    "correct" : "Earthquake",
+    "voice": "./voices/ques-7.mp3"
+    },
+
+    {
+    "question" : "'This will not be an easy mission, _____ slow the expedition.'",
+    "option1" : "Plants",
+    "option2" : "Hunters",
+    "option3" : "Monkeys",
+    "option4" : "Farmers",
+    "correct" : "Monkeys",
+    "voice": "./voices/ques-8.mp3"
+    },
+
+    {
+    "question" : "A _____ from the darkest wild makes you feel just like a child.",
+    "option1" : "Rider",
+    "option2" : "Hunter",
+    "option3" : "Toy",
+    "option4" : "Thrower",
+    "correct" : "Hunter",
+    "voice": "./voices/ques-9.mp3"
+    },
+
+    {
+    "question" : "Beware the ground on which you stand. The floor is quicker than the sand.",
+    "option1" : "Ocean",
+    "option2" : "Ground",
+    "option3" : "Quicksand",
+    "option4" : "Storm",
+    "correct" : "Quicksand",
+    "voice": "./voices/ques-10.mp3"
+    },
+
+    {
+    "question" : "What goes up but never comes down?",
+    "option1" : "Age",
+    "option2" : "Chimney",
+    "option3" : "Santa",
+    "option4" : "Birds",
+    "correct" : "Age",
+    "voice": "./voices/ques-11.mp3"
+    },
+
+    {
+        "question" : "They come out at night without being called, and are lost in the day without being stolen. What are they?",
+        "option1" : "Constelations",
+        "option2" : "Moon",
+        "option3" : "Star",
+        "option4" : "Night",
+        "correct" : "Star",
+        "voice": "./voices/ques-12.mp3"
+        },
+
+    {
+        "question" : "We see it once in a year, twice in a week, and never in a day. What is it?",
+        "option1" : "The Letter Y",
+        "option2" : "The Letter E",
+        "option3" : "The Letter F",
+        "option4" : "The Letter A",
+        "correct" : "The Letter E",
+        "voice": "./voices/ques-13.mp3"
+    },
+
+    {
+        "question" : "Which weighs more, a pound of feathers or a pound of bricks?",
+        "option1" : "Brick",
+        "option2" : "Pound",
+        "option3" : "Feathers",
+        "option4" : "Both",
+        "correct" : "Both",
+        "voice": "./voices/ques-14.mp3"
+    },
+
+    {
+        "question" : "What has one eye but cannot see?",
+        "option1" : "Clock",
+        "option2" : "Needle",
+        "option3" : "Pin",
+        "option4" : "Thread",
+        "correct" : "Pin",
+        "voice": "./voices/ques-15.mp3"
+    },
+
+    {
+        "question" : "A man was driving his truck. His lights were not on. The moon was not out. Up ahead, a woman was crossing the street. How did he see her?",
+        "option1" : "Sunny Day",
+        "option2" : "Its Dark",
+        "option3" : "Morning",
+        "option4" : "Night",
+        "correct" : "Sunny Day",
+        "voice": "./voices/ques-16.mp3"
+    },
+
+    {
+        "question" : "What is so delicate that saying its name breaks it?",
+        "option1" : "Voice",
+        "option2" : "Silent",
+        "option3" : "Quiet",
+        "option4" : "Silence",
+        "correct" : "Silence",
+        "voice": "./voices/ques-17.mp3"
+    },
+
+    {
+        "question" : "What is as light as a feather, but even the world’s strongest man couldn’t hold it for more than a minute?",
+        "option1" : "A feather",
+        "option2" : "A breath",
+        "option3" : "A piece of paper",
+        "option4" : "A plastic bag",
+        "correct" : "A breath",
+        "voice": "./voices/ques-18.mp3"
+    },
+
+    {
+        "question" : "A house has 4 walls. All of the walls are facing south, and a bear is circling the house. What color is the bear?",
+        "option1" : "Pink",
+        "option2" : "White",
+        "option3" : "Purple",
+        "option4" : "Orange",
+        "correct" : "White",
+        "voice": "./voices/ques-19.mp3"
+    },
+
+    {
+        "question" : "Take away my first letter, and I still sound the same. Take away my last letter, I still sound the same. Even take away my letter in the middle, I will still sound the same. I am a five letter word. What am I?",
+        "option1" : "A secr5t",
+        "option2" : "A whisper  ",
+        "option3" : "A paper",
+        "option4" : "A secret",
+        "correct" : "A secret",
+        "voice": "./voices/ques-20.mp3"
+    },
+
+    {
+        "question" : "If I have it, I don’t share it. If I share it, I don’t have it. What is it??",
+        "option1" : "Empty",
+        "option2" : "Something ",
+        "option3" : "You",
+        "option4" : "Nothing",
+        "correct" : "Empty",
+        "voice": "./voices/ques-21.mp3"
+    },
+
+    {
+        //22
+        "question" : "What travels around the world but stays in one spot?",
+        "option1" : "Star",
+        "option2" : "Stamp ",
+        "option3" : "Moon",
+        "option4" : "Postcard",
+        "correct" : "Stamp",
+        "voice": "./voices/ques-22.mp3"
+    },
+
+    {
+        "question" : "I am a mother's child and a father's son. Who am I ?",
+        "option1" : "Grandson",
+        "option2" : "Daughter",
+        "option3" : "Father",
+        "option4" : "Uncle",
+        "correct" : "Daughter",
+        "voice": "./voices/ques-23.mp3"
+    },
+
+    {
+        "question" : "When i get multiplied by any number, the sum of the figures in the product is always me. What am I ?",
+        "option1" : "9",
+        "option2" : "8",
+        "option3" : "2",
+        "option4" : "4",
+        "correct" : "9",
+        "voice": "./voices/ques-24.mp3"
+    },
+
+    {
+        "question" : "If a giraffe has two eyes, a monkey has two eyes, and an elephant has two eyes, how many eyes do we have?",
+        "option1" : "1",
+        "option2" : "2",
+        "option3" : "3",
+        "option4" : "4",
+        "correct" : "4",
+        "voice": "./voices/ques-25.mp3"
+    },
+
+    {
+        "question" : "How many times in a day, are the hands of a clock in straight line but opposite in direction?",
+        "option1" : "20",
+        "option2" : "22",
+        "option3" : "24",
+        "option4" : "48",
+        "correct" : "22",
+        "voice": "./voices/ques-26.mp3"
+    },
+
+    {
+        "question" : "One rabbit saw 6 elephants while going towards River. Every elephant saw 2 monkeys are going towards river. Every monkey holds one tortoice in their hands.How many animals are going towards the river?",
+        "option1" : "14",
+        "option2" : "11",
+        "option3" : "5",
+        "option4" : "8",
+        "correct" : "5",
+        "voice": "./voices/ques-27.mp3"
+    },
+]
+
+//Preparing The Question
+
+let questions = []
+totalQ = 0;
+indexes = []
+
+//Inserting 10 Ques Randomly
+
+while(totalQ != 10)
+{
+    let contains = false;
+    let quesNo = Math.floor(Math.random()*(quiz.length-1));
+    for(let i=0;i<indexes.length;i++){
+        if(quesNo === indexes[i]){
+            contains = true;
+        }
+    }
+
+    if(!contains){
+        indexes.push(quesNo)
+        questions.push(quiz[quesNo])
+        totalQ++;
+    }
+}
+
+//Player Questions Details
+
+let currentQues = 0;
+let selectedans = ""
+
+let player = {
+    name:"NONE",
+    score:0,
+}
+
+let result_box = []
+
+//Music Additions
+
+let song = new Audio(questions[currentQues].voice)
+
+let option_select = new Audio("./voices/option-select.mp3")
+let next = new Audio("./voices/next.mp3")
+
+document.querySelector("#name-selection").addEventListener("change",(e)=>{
+    player.name = e.target.value
+})
+
+const game_box = document.querySelector("#game-container")
+
+//Lets-Play Event
+
+document.querySelector("#lets-play").addEventListener("click",()=>{
+    document.querySelector("#starting-box").classList.add("display")
+    document.querySelector("#game-container").classList.remove("display")
+    optionBox()
+})
+
+//Show Final Result
+
+function showResult()
+{
+    for(let i=0;i<result_box.length;i++)
+    {
+        let right = ""
+        if(result_box[i].point === 1){
+            right = "correctAns";
+        }
+
+        document.querySelector(".right-end").innerHTML = document.querySelector(".right-end").innerHTML +
+        `<div class="ques">
+            <div class="question">
+                ${result_box[i].question}
+            </div>
+            <div class="y-answer">
+                ${result_box[i].youranswer}
+            </div>
+            <div class="c-answer">
+                ${result_box[i].answer}
+            </div>
+            <div class="r-answer ${right}">
+                ${result_box[i].point}
+            </div>
+        </div>`;
+    }
+}
+
+//Next-Question
+
+const nextQues = () =>{
+
+    console.log(questions[currentQues].question)
+        console.log(questions[currentQues].correct)
+        console.log(selectedans)
+
+        result_box.push({
+            "question":questions[currentQues].question,
+            "answer":questions[currentQues].correct,
+            "youranswer":selectedans,
+            "point": (selectedans === questions[currentQues].correct ? 1 : 0)
+            })
+
+        currentQues++; 
+        
+        if(currentQues < 10)
+        {
+            song.pause();
+            song = new Audio(questions[currentQues].voice) 
+        }
+        
+        if(selectedans == questions[currentQues-1].correct){
+            player.score += 1;
+        }   
+
+        if(currentQues == 10)
+        {
+            //Compliment
+            let compliment = ""
+
+            if(player.score === 10){
+                compliment = "High IQ"
+            }
+
+            else  if(player.score >= 7 && player.score < 10){
+                compliment = "Good Going !!"
+            }
+
+            else  if(player.score >= 5 && player.score < 7){
+                compliment = "Keep It Up !!"
+            }
+
+            else  if(player.score >= 3 && player.score < 5){
+                compliment = "Not Good"
+            }
+
+            else{
+                compliment = "Better Luck Next Time"
+            }
+
+            game_box.innerHTML = " "
+            game_box.innerHTML = `
+                                <div class="end-box">
+                                    <div class="left-end-box">
+                                        <h1 class="your-score">${player.name} Scored</h1>
+                                        <h1 class="score">${player.score} / ${result_box.length}</h1>
+                                        <p class="compliment">${compliment}</p>
+                                    </div>
+
+                                    <div class="right-end">
+                                    </div>  
+                                </div>`
+                                showResult();
+            return;
+        }
+
+        game_box.innerHTML = " ";
+        game_box.innerHTML = ` <div class="ques-box">
+                                    <h1>Ques - ${currentQues+1} ${questions[currentQues].question}</h1>
+                                </div>
+                                <div class="option-box">
+                                    <div class="row">
+                                        <button class="option-button">${ questions[currentQues].option1 }</button>
+                                        <button class="option-button">${ questions[currentQues].option2 }</button>
+                                    </div>
+                                    <div class="row">
+                                        <button class="option-button">${ questions[currentQues].option3 }</button>
+                                        <button class="option-button">${ questions[currentQues].option4 }</button>
+                                    </div>
+                                </div>
+                                <button class="next-btn display">Next</button>`
+        song.play()
+        optionBox()
+}
+
+
+//Option - Selection
+
+const optionBox = () =>{
+    
+    document.querySelector('#game-container').innerHTML =
+                                                    `<div class="ques-box">
+                                                        <h1>Ques - ${currentQues+1} ${questions[currentQues].question}</h1>
+                                                    </div>
+                                                    <div class="option-box">
+                                                        <div class="row">
+                                                            <button class="option-button">${questions[currentQues].option1}</button>
+                                                            <button class="option-button">${questions[currentQues].option2}</button>
+                                                        </div>
+                                                        <div class="row">
+                                                            <button class="option-button">${questions[currentQues].option3}</button>
+                                                            <button class="option-button">${questions[currentQues].option4}</button>
+                                                        </div>
+                                                    </div>
+                                                    <button class="next-btn display">Next</button>`
+
+    song.play();
+
+    const options = document.querySelectorAll(".option-button")
+
+        for(let i=0;i<4;i++)
+        {
+            options[i].addEventListener("click", ()=>{
+
+                option_select.pause();
+                song.pause()
+                option_select.play();
+
+                selectedans = options[i].innerHTML
+
+                for(let j=0;j<4;j++)
+                {
+                    options[j].classList.remove("selected-btn")
+                }
+
+                options[i].classList.add("selected-btn")
+                document.querySelector(".next-btn").classList.remove("display")
+
+                
+            })
+        }
+
+        document.querySelector(".next-btn").addEventListener("click",()=>{
+            nextQues()
+        })
+}
+
